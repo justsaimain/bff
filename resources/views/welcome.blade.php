133ebb9f5d0a90bf072fafd0830d679e663561c9
justsaimain <saimain.primary@gmail.com>
2021-12-31 16:10:25 +0630

    prevent mobile zoomable

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="viewport" content="width=device-width, initial-scale=1 , minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no">
         <title>{{ env('APP_NAME') }}</title>
 
         <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
