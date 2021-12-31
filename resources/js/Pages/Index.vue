e0e6e4999b5c370f39442c268c1c96690a47adca
justsaimain <saimain.primary@gmail.com>
2021-12-31 15:56:14 +0630

    init commit

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,278 @@
+<template>
+    <div class="top-div">
+        <div class="mb-3 d-flex justify-content-between align-items-center">
+            <h3 class="title">Burmese Fantasy</h3>
+            <a href="#" class="text-white">
+                <i class="bx bxs-bell" style="font-size: 25px"></i>
+            </a>
+        </div>
+        <div class="header-div text-center">
+            <p>Welcome Sai Main</p>
+            <div class="card border-0 shadow-sm">
+                <div class="card-body text-dark rounded">
+                    <p class="game-week-title">Game Week 1 Result</p>
+                    <div
+                        class="d-flex justify-content-around align-items-center"
+                    >
+                        <div>
+                            <h3 class="result-number">42</h3>
+                            <span>Average</span>
+                        </div>
+                        <div>
+                            <h3 class="result-number your-score">42</h3>
+                            <span>Your Score</span>
+                        </div>
+                        <div>
+                            <h3 class="result-number">144</h3>
+                            <span>Highest</span>
+                        </div>
+                    </div>
+                    <hr />
+                    <div
+                        class="d-flex justify-content-between align-items-center"
+                    >
+                        <span>Lorem ipsum dolor sit amet.</span>
+                        <button class="btn btn-primary btn-sm">
+                            View Your Point
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="body-div mt-3">
+            <p class="text-dark">Recent Matches</p>
+            <div class="recent-matches-container">
+                <div class="card border-0 shadow-sm recent-matches">
+                    <div
+                        class="card-body d-flex justify-content-between align-items-center"
+                    >
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t3.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Arsenal</span>
+                        </div>
+                        <div
+                            class="d-flex align-items-center justify-content-center"
+                        >
+                            <h3 class="text-dark">5 : 2</h3>
+                        </div>
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t8.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Chelsea</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card border-0 shadow-sm recent-matches">
+                    <div
+                        class="card-body d-flex justify-content-between align-items-center"
+                    >
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t3.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Arsenal</span>
+                        </div>
+                        <div
+                            class="d-flex align-items-center justify-content-center"
+                        >
+                            <h3 class="text-dark">5 : 2</h3>
+                        </div>
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t8.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Chelsea</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card border-0 shadow-sm recent-matches">
+                    <div
+                        class="card-body d-flex justify-content-between align-items-center"
+                    >
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t3.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Arsenal</span>
+                        </div>
+                        <div
+                            class="d-flex align-items-center justify-content-center"
+                        >
+                            <h3 class="text-dark">5 : 2</h3>
+                        </div>
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t8.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Chelsea</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card border-0 shadow-sm recent-matches">
+                    <div
+                        class="card-body d-flex justify-content-between align-items-center"
+                    >
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t3.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Arsenal</span>
+                        </div>
+                        <div
+                            class="d-flex align-items-center justify-content-center"
+                        >
+                            <h3 class="text-dark">5 : 2</h3>
+                        </div>
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t8.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Chelsea</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card border-0 shadow-sm recent-matches">
+                    <div
+                        class="card-body d-flex justify-content-between align-items-center"
+                    >
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t3.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Arsenal</span>
+                        </div>
+                        <div
+                            class="d-flex align-items-center justify-content-center"
+                        >
+                            <h3 class="text-dark">5 : 2</h3>
+                        </div>
+                        <div
+                            class="d-flex flex-column align-items-center justify-content-center"
+                        >
+                            <img
+                                src="https://resources.premierleague.com/premierleague/badges/70/t8.png"
+                                alt=""
+                                width="40"
+                            />
+                            <span class="text-dark">Chelsea</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import VueHorizontal from "vue-horizontal";
+export default {
+    components: { VueHorizontal },
+    data() {
+        return {
+            recent_matches: [{}],
+        };
+    },
+};
+</script>
+
+<style scoped>
+* {
+    font-family: sans-serif;
+}
+.top-div {
+    background: var(--first-color);
+    color: #fff;
+    padding: 2rem 1rem;
+    border-radius: 0 0 2rem 2rem;
+    max-height: 200px;
+}
+
+.title {
+    font-weight: 600;
+    font-size: 18px;
+    font-family: sans-serif;
+}
+
+.header-div p {
+    font-weight: 400;
+    font-size: 16px;
+}
+
+.game-week-title {
+    font-size: 15px;
+}
+
+.result-number {
+    font-size: 20px;
+}
+
+.your-score {
+    font-size: 25px;
+    color: var(--first-color);
+    font-weight: bold;
+}
+
+.horizontal >>> .v-hl-btn {
+    display: none;
+}
+
+.recent-matches-container {
+    display: flex;
+    overflow-x: auto;
+}
+
+.recent-matches {
+    margin-right: 10px;
+    min-width: 300px;
+}
+
+/* Hide scrollbar for Chrome, Safari and Opera */
+.recent-matches-container::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.recent-matches-container {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+</style>
